f5aac36c99cbc304b70565325c354d1f6d8ea661
puknotbad <95931471+puknotbad@users.noreply.github.com>
2023-01-17 16:39:45 +0300

    Update 12.7. Algo_HW_IsKPeriodic.cpp
    Updated description of the function.

--- 12.7. Algo_HW_IsKPeriodic.cpp
+++ 12.7. Algo_HW_IsKPeriodic.cpp
@@ -6,8 +6,7 @@
 //  Description :   Skillfactory C++ 12.7 home work: IsKPeriodic function realisation. 
 //                  Checks, wheather a string is k-periodic or not.
 //                  Algorithm splits the input string into two halves and compares them
-//                  every time until one halve becomes k-length. At the end it compares
-//                  the rest substring with the k-length prefix of the input string.
+//                  every time until one halve becomes k-length prefix of the input string.
 // ------------------------------------------------------------------------------------
 //              k-length prefix
 //                  /  \
